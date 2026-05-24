7b99dd0774e7ab487f204fba2749aa870dc3ec20
mehr <mehrfarooq891@gmail.com>
2026-05-24 17:37:03 +0500

    feat: add blog post about soil survey costs
    Create and register a new blog page covering soil survey (zemin etüdü) requirements, procedures, and 2026 cost estimates to expand informational content.

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,7 @@ export default defineConfig(({mode}) => {
           blog_izmir: path.resolve(__dirname, 'blog-izmir-2026.html'),
           blog_muteahhit: path.resolve(__dirname, 'blog-muteahhit-mi-kendim-mi.html'),
           blog_ruhsat: path.resolve(__dirname, 'blog-insaat-ruhsati-maliyeti-2026.html'),
+          blog_zemin: path.resolve(__dirname, 'blog-zemin-etudu-maliyeti.html'),
           blog: path.resolve(__dirname, 'blog.html'),
         },
       },
