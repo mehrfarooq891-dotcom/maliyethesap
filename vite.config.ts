897718569dabf55df306925caa8008ef00290be1
mehr <mehrfarooq891@gmail.com>
2026-06-20 11:55:39 +0500

    feat: add Ankara house construction cost 2026 guide
    Include a new blog post regarding 2026 construction costs in Ankara and update the site configuration and main blog page to support the new entry.

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig(({mode}) => {
           blog_insaat_demiri: path.resolve(__dirname, 'blog-insaat-demiri-fiyatlari-2026.html'),
           blog_cimento: path.resolve(__dirname, 'blog-cimento-fiyatlari-2026.html'),
           blog_insaat_maliyeti_rehberi: path.resolve(__dirname, 'blog-insaat-maliyeti-hesaplama-rehberi.html'),
+          blog_ankara_ev_yapim_maliyeti: path.resolve(__dirname, 'blog-ankara-ev-yapim-maliyeti-2026.html'),
           blog_insaat_maliyeti: path.resolve(__dirname, 'blog-insaat-maliyeti-hesaplama.html'),
           blog_luks_ekonomik: path.resolve(__dirname, 'blog-luks-ekonomik-insaat-farki.html'),
           blog_ankara: path.resolve(__dirname, 'blog-ankara-2026.html'),
